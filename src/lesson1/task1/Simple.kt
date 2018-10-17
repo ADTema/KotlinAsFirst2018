7dbbbf74282d62b0dc2e5db1ab09408bcc1b4886
ADTema <adavladvartem@yandex.ru>
2018-10-17 12:30:49 +0300

    lesson3

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -1,4 +1,5 @@
 @file:Suppress("UNUSED_PARAMETER")
+
 package lesson1.task1
 
 
@@ -85,7 +86,7 @@ fun angleInRadian(grad: Int, min: Int, sec: Int): Double = PI / 180 * (grad + mi
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt ( sqr(x1 - x2) + sqr(y1 - y2 ))
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(x1 - x2) + sqr(y1 - y2))
 
 /**
  * Простая
@@ -93,7 +94,7 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt (
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = (( number / 10 ) / 10 ) % 10
+fun thirdDigit(number: Int): Int = ((number / 10) / 10) % 10
 
 /**
  * Простая
@@ -120,4 +121,4 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = (number % 10) * 100 + ( ( number / 10) % 10 ) * 10 + number / 100
+fun numberRevert(number: Int): Int = (number % 10) * 100 + ((number / 10) % 10) * 10 + number / 100
